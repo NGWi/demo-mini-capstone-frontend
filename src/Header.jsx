223f9f662b6bbb61e167092e55c37e1ddc212440
Nathan G. Wiseman <55075327+NGWi@users.noreply.github.com>
2024-08-02 12:54:34 -0400

    Added header syltes

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,11 +3,14 @@ import { LogoutLink } from "./LogoutLink";
 
 export function Header() {
   return (
-    <header>
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/products">All products</Link> |{" "}
-        <Link to="/products/new">New product</Link> | <Link to="/signup">Signup</Link> | <Link to="/login">Login</Link>{" "}
-        | <LogoutLink />
+    <header className="bg-gray-200 py-4">
+      <nav className="container mx-auto flex justify-between items-center">
+        <Link to="/" className="text-blue-500 hover:text-blue-800">Home</Link> | 
+        <Link to="/products" className="text-blue-500 hover:text-blue-800">All products</Link> | 
+        <Link to="/products/new" className="text-blue-500 hover:text-blue-800">New product</Link> | 
+        <Link to="/signup" className="text-blue-500 hover:text-blue-800">Signup</Link> | 
+        <Link to="/login" className="text-blue-500 hover:text-blue-800">Login</Link> | 
+        <LogoutLink />
       </nav>
     </header>
   );
